223480538807bcc6ce8c6bb2d957fb6826f840fa
Dariusz Gawlak <gawdar73@gmail.com>
2024-02-02 18:24:08 +0100

    Zadanie_Dzien_4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Homework day 4
+
+var age = 50;
+var sex = "kobieta";
+var name = "Ewa";
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+
+    
+
